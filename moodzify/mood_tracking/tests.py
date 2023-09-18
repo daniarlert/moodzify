6bca094e750c091fb522c59a2c7b15b7878ca2af
daniarlert <24715931+daniarlert@users.noreply.github.com>
2023-09-18 11:56:27 +0200

    test: add moods and users basic testing

--- moodzify/mood_tracking/tests.py
+++ moodzify/mood_tracking/tests.py
@@ -1,1 +1,55 @@
-# Create your tests here.
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+from .models import MoodEntry
+
+
+class MoodEntryModelTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="testuser", email="test@example.com", password="testpassword"
+        )
+
+        self.mood_entry = MoodEntry.objects.create(
+            user=self.user,
+            mood_level=1,  # Positive
+            notes="Feeling great!",
+        )
+
+    def test_mood_entry_creation(self):
+        """
+        Test that a mood entry was created successfully.
+        """
+        self.assertEqual(self.mood_entry.user, self.user)
+        self.assertEqual(self.mood_entry.mood_level, 1)  # Positive
+        self.assertEqual(self.mood_entry.notes, "Feeling great!")
+
+    def test_string_representation(self):
+        """
+        Test the string representation of the mood entry.
+        """
+        self.assertEqual(str(self.mood_entry), str(self.mood_entry.timestamp))
+
+    def test_mood_level_choices(self):
+        """
+        Test that mood_level choices are valid.
+        """
+        valid_choices = [-2, -1, 0, 1, 2]
+        for choice in valid_choices:
+            mood_entry = MoodEntry.objects.create(
+                user=self.user,
+                mood_level=choice,
+                notes=f"Test entry with mood level {choice}",
+            )
+            self.assertIn(mood_entry.mood_level, valid_choices)
+
+    def test_timestamp_auto_add(self):
+        """
+        Test that the timestamp is automatically added.
+        """
+        mood_entry = MoodEntry.objects.create(
+            user=self.user,
+            mood_level=0,  # Neutral
+            notes="Testing timestamp auto add",
+        )
+        self.assertIsNotNone(mood_entry.timestamp)
--- moodzify/users/tests.py
+++ moodzify/users/tests.py
@@ -1,1 +1,56 @@
-# Create your tests here.
+from django.test import TestCase
+
+from .models import CustomUser
+
+
+class CustomUserModelTest(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            username="testuser", email="test@example.com", password="testpassword"
+        )
+
+    def test_user_creation(self):
+        """
+        Test that a user was created successfully.
+        """
+
+        self.assertEqual(self.user.username, "testuser")
+        self.assertEqual(self.user.email, "test@example.com")
+        self.assertTrue(self.user.check_password("testpassword"))
+
+    def test_string_representation(self):
+        """
+        Test the string representation of the user.
+        """
+        self.assertEqual(str(self.user), "testuser")
+
+    def test_user_authentication(self):
+        """
+        Test user authentication.
+        """
+        user = CustomUser.objects.get(username="testuser")
+        self.assertTrue(user.check_password("testpassword"))
+        self.assertFalse(user.check_password("wrongpassword"))
+
+    def test_superuser_creation(self):
+        """
+        Test creating a superuser.
+        """
+        admin_user = CustomUser.objects.create_superuser(
+            username="adminuser", email="admin@example.com", password="adminpassword"
+        )
+        self.assertTrue(admin_user.is_superuser)
+        self.assertTrue(admin_user.is_staff)
+
+    def test_user_permissions(self):
+        """
+        Test user permissions.
+        """
+        user = CustomUser.objects.get(username="testuser")
+        self.assertFalse(user.is_superuser)
+        self.assertFalse(user.is_staff)
+
+        user.is_staff = True
+        user.save()
+
+        self.assertTrue(user.is_staff)
